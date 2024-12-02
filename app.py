1962d09e7b4acb8a25e9dca05a25f58c7ddc5e3c
Lagnajeet-CG <121150082+Lagnajeet-CG@users.noreply.github.com>
2024-12-02 12:51:36 +0530

    Update app.py
    image format change line 113

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ st.markdown("""
 st.markdown('<h1>Strategic AI Guidance Engine🤖</h1>', unsafe_allow_html=True)
 
 # # Path to your logo
-logo_path2 = "SAGE2.jpg"
+logo_path2 = "SAGE2.png"
 
 # Display the logo in the sidebar
 st.sidebar.image(logo_path2, use_column_width=True)
